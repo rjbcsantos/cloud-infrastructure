d3f03bb8a40ac91f1d73c19bc6dc7e6b75a5464a
Rodrigo Santos <rodrigo.santos@acqio.com.br>
2023-08-27 21:47:30 -0300

    Add script to start a local kubernetes cluster and deploy the artifacts

---
+++ local-minikube-setup.sh
@@ -0,0 +1,28 @@
+# Fail fast.
+set -e
+
+# Check that kubectl is installed.
+which kubectl > /dev/null || {
+    echo 'ERROR: kubectl is not installed';
+    exit 1;
+}
+
+# Check that minikube is installed.
+which minikube > /dev/null || {
+    echo 'ERROR: minikube is not installed';
+    exit 1;
+}
+
+# Start the minikube-multinode with 3 nodes.
+minikube start --nodes 3 -p minikube-multinode --kubernetes-version v1.26.3 --driver=docker
+
+# Fix the kubectl context, as it's often stale.
+minikube update-context
+
+# Display the cluster informations.
+echo "Get cluster details to check its running"
+kubectl get nodes
+kubectl cluster-info
+
+# Deploy artifacts to the kubernetes cluster.
+kubectl apply -f kube/
